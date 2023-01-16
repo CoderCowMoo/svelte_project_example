ad1d636f4412d7413d01a0635e024f73a004b054
CoderCowMoo <broskichad38@gmail.com>
2023-01-16 21:50:24 +1100

    Project

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  let text_value = '';
+
+  $: fucks = (text_value.match(/fuck/g) || []).length;
+</script>
+
+<main>
+  <h1>Hello from Svelte</h1>
+  <input bind:value={text_value} />
+  <p>{fucks}</p>
+  <p>What a beautiful framework</p>
+</main>
